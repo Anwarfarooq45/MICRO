df5edc9b4131153d83dff1f586f95b5ab3538c48
Anwarfarooq45 <anwarfarooq45@gmail.com>
2024-09-04 10:33:35 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
-$username = "root";
-$password = "";
-$database = "micro";
+$username = "if0_37229673";
+$password = "Microweigh30";
+$database = "if0_37229673_customer";
 
-$conn = new mysqli("localhost", $username, $password, $database);
+$conn = new mysqli("sql108.infinityfree.com", $username, $password, $database);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
